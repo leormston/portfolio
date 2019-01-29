e0a24f39a39eb81fb00338cd711520c6333e91d3
louie <l.e.ormston@gmail.com>
2019-01-29 10:34:55 +0000

    P.0.4 -Homepage Complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,72 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load ', function(){
+  $("#typewriter").hide();
+  $("#discovertext").hide();
+  $("#createdtext").hide();
+  $("#whattext").hide();
+  $("#ivetext").hide();
+  $("#discovertext").fadeIn(800);
+  $("#createdtext").fadeIn(1500);
+  $("#whattext").fadeIn(2000);
+  $("#ivetext").fadeIn(2000);
+  $("#color3").css("background-color", "purple");
+  $("#color2").css("background-color", "lightgreen");
+});
+
+var i = 0;
+var txt = 'Welcome to my site potential employer. Come and view what I have created :)';
+var speed = 75;
+
+function typeWriter() {
+  if (i < txt.length) {
+  document.getElementById("typexxx").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function changecolour(element, colour) {
+  if (colour == "0") {
+    var number = 100000 + Math.floor(Math.random() * 899999);
+    var n = number.toString();
+    $("#" + element).css("background-color", "#" + n)
+    $("#" + element).css("border", "solid 5px #" + n)
+  } else {
+    $("#" + element).css("background-color", "#" + colour)
+    $("#" + element).css("border", "solid 5px #" + colour)
+  }
+
+}
+
+function typeHide() {
+  $("#typewriter").fadeOut("slow");
+  $(".showbutton").fadeIn("fast");
+}
+
+function typeShow() {
+  $(".showbutton").hide();
+  $("#typewriter").fadeIn("800");
+}
+
+async function changecoloursequence(changer) {
+  var number1 = 100000 + Math.floor(Math.random() * 899999);
+  var number2 = 100000 + Math.floor(Math.random() * 899999);
+  var number3 = 100000 + Math.floor(Math.random() * 899999);
+  var number4 = 100000 + Math.floor(Math.random() * 899999);
+  var n1 = number1.toString();
+  var n2 = number2.toString();
+  var n3 = number3.toString();
+  var n4 = number4.toString();
+
+  setTimeout(changecolour("color", 0), 1000);
+  changecolour("color2", 0);
+  changecolour("color3", 0);
+  changecolour("color4", 0);
+  $("#" + changer).fadeOut(1000);
+}
